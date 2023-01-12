0c6419b39501ce01787500aeac191317ac98ff82
aibyn.sagyndyk <aibyn.sagyndyk@astanait.edu.kz>
2023-01-12 11:37:47 +0600

    Updating URL for front-end

--- src/main/java/kz/edu/astanait/projectacceptanceplatform/controller/AuthController.java
+++ src/main/java/kz/edu/astanait/projectacceptanceplatform/controller/AuthController.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "https://project-acceptance-front.herokuapp.com/", maxAge = 3600)
+@CrossOrigin(origins = "diploma-projects.up.railway.app/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
